b7e2753695edf2104c73a0734f6ab24c158b5a4e
Jim Fitzgerald <dumples5@gmail.com>
2014-10-04 18:38:15 -0400

    added html5mode

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var app = angular.module('mySite', ['ui.router']);
 
+app.config(function($locationProvider) {
+  $locationProvider.html5Mode(true);
+});
+
 app.controller('HomeController', ['$scope', function($scope) {
   $scope.greeting = "Hola!!";
 }]);
