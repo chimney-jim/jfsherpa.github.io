8c2b31e91ddd77dd66246ee21370d3f399c72042
Jim Fitzgerald <dumples5@gmail.com>
2014-10-04 18:45:45 -0400

    added first route for home page

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var app = angular.module('mySite', ['ui.router']);
 
 app.config(function($locationProvider) {
   $locationProvider.html5Mode(true);
+
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "partials/home.html",
+      controller: 'HomeController'
+    });
 });
 
 app.controller('HomeController', ['$scope', function($scope) {
