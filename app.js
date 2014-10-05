fbc46e11e295331af5fc3f2b21dee6dfdc4c9e1b
Jim Fitzgerald <dumples5@gmail.com>
2014-10-05 14:49:54 -0400

    forgot to include the injector for ngRoute

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('mySite', ['ui.router']);
+var app = angular.module('mySite', ['ngRoute']);
 
 app.config(function($locationProvider, $routeProvider) {
   $locationProvider.html5Mode(true);
